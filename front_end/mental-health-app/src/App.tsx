d38d7b7c9b3a888bc1d19573f468c1a0b48cab5e
chelseakari <sflake1225@gmail.com>
2024-05-24 22:03:11 -0700

    Completed setup of frontend

---
+++ front_end/mental-health-app/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ButtonUsage from "./components/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <ButtonUsage />
+      </div>
+    </>
+  );
+}
+
+export default App;
